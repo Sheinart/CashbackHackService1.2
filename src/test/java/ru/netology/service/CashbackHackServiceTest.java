5d168eec6041267694820a72b6d1d7f379eab28b
Sheinart <sheinart09@gmail.com>
2023-07-25 17:00:47 +0300

    testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,7 +6,25 @@ import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @Test
-    public void testRemain() {
+    @org.testng.annotations.Test
+    public void testThereIsEnoughMoney() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+
     }
-}
+
+    @org.testng.annotations.Test
+    public void testAddMoney() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 630;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 370;
+
+        assertEquals(actual, expected);
+    }}
